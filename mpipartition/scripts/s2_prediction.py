dfc45fec2a7506e533e2eef8a6bdc97a86a586e8
Michael Buehlmann <buehlmann.michi@gmail.com>
2023-06-08 17:43:53 -0500

    add S2 visualizations and CLI

---
+++ mpipartition/scripts/s2_prediction.py
@@ -0,0 +1,80 @@
+import sys
+from typing import Optional
+
+import click
+
+from mpipartition.spherical_partition.s2_partition import (
+    _s2_partition,
+    visualize_s2_partition,
+    _build_s2_segment_list,
+    _print_area_imabalance,
+    _print_edge_to_area_ratio,
+    _print_segmentation_info,
+)
+
+
+@click.command()
+@click.argument(
+    "nranks",
+    type=int,
+)
+@click.option(
+    "--equal-area",
+    is_flag=True,
+    help=(
+        "If set, partition S2 into equal area segments by varying delta_theta of "
+        "rings."
+    ),
+)
+@click.option(
+    "--precision",
+    type=int,
+    default=6,
+    help="Number of decimal places to use for printing.",
+)
+@click.option(
+    "--figure",
+    type=click.Path(dir_okay=False, writable=True),
+    help="If set, save a visual representation of the S2 partitioning to this file.",
+)
+@click.option(
+    "--use-mollweide",
+    is_flag=True,
+    help=(
+        "If set, use the Mollweide projection. Otherwise, use a rectangular theta-phi "
+        "plot for visualization."
+    ),
+)
+def cli(
+    nranks: int,
+    equal_area: bool,
+    precision: int,
+    figure: Optional[str],
+    use_mollweide: bool,
+):
+    theta_cap, ring_thetas, ring_segments = _s2_partition(nranks, equal_area)
+    all_s2_segments = _build_s2_segment_list(theta_cap, ring_thetas, ring_segments)
+    _print_segmentation_info(
+        nranks,
+        theta_cap,
+        ring_thetas,
+        ring_segments,
+        precision=precision,
+    )
+    _print_area_imabalance(all_s2_segments, precision=precision)
+    _print_edge_to_area_ratio(all_s2_segments, precision=precision)
+
+    if figure is not None:
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            print(
+                "matplotlib is required for visualization, install mpipartition with "
+                "`pip install mpipartition[viz]",
+                file=sys.stderr,
+            )
+            return
+        fig, ax = visualize_s2_partition(nranks, equal_area, use_mollweide)
+        if use_mollweide:
+            ax.set(xticklabels=[], yticklabels=[])
+        fig.savefig(figure)
--- mpipartition/spherical_partition/s2_partition.py
+++ mpipartition/spherical_partition/s2_partition.py
@@ -20,10 +20,12 @@ def _cap_angle(area):
     return np.arccos(1 - h)
 
 
-def _s2_partition(n, adjust_theta=True, verbose=False):
+def _s2_partition(n, adjust_theta=True):
     # Following the algorithm by Paul Leopardi (https://eqsp.sourceforge.net/)
     # TODO: generalize to arbitrary number of dimensions
-    assert n > 2
+    assert n > 1
+    if n == 2:
+        return np.pi / 2, np.array([], dtype=np.float64), np.array([], dtype=np.int32)
 
     target_area = 4 * np.pi / n
 
@@ -33,8 +35,7 @@ def _s2_partition(n, adjust_theta=True, verbose=False):
     # 2-4: rings
     delta_i = np.sqrt(target_area)  # ideal spacing
     n_rings_i = (np.pi - 2 * theta_c) / delta_i
-    n_rings = int(n_rings_i + 0.5)
-
+    n_rings = max(int(n_rings_i + 0.5), 1)
     # 5-6: ring segments
     delta_f = (np.pi - 2 * theta_c) / n_rings
     theta_f = theta_c + np.arange(n_rings) * delta_f
@@ -56,16 +57,6 @@ def _s2_partition(n, adjust_theta=True, verbose=False):
         areas = target_area * np.array(n_segments)
         cum_areas = target_area + np.cumsum(areas)
         theta_f[1:] = _cap_angle(cum_areas)
-
-    if verbose:
-        print(f"Segmentation statistics for {n} ranks:")
-        print(f"  polar cap angle: {theta_c:.3f}")
-        print(f"  number of rings: {n_rings}")
-        for i in range(n_rings):
-            print(
-                f"    ring {i:3d}: {n_segments[i]:3d} segments "
-                f"between theta=[{theta_f[i]:.3f}, {theta_f[i+1]:.3f}]]"
-            )
 
     return theta_c, theta_f, n_segments
 
@@ -99,9 +90,15 @@ def _build_s2_segment_list(theta_cap, ring_thetas, ring_segments):
         edge_length = (
             edge_length_top + edge_length_bottom + 2 * (theta_end - theta_start)
         )
-        segments.append(
-            S2Segment((theta_start, theta_end), phi_edges, area, edge_length)
-        )
+        for j in range(ring_segments[i]):
+            segments.append(
+                S2Segment(
+                    (theta_start, theta_end),
+                    (phi_edges[j], phi_edges[j + 1]),
+                    area,
+                    edge_length,
+                )
+            )
 
     # cap
     segments.append(
@@ -111,19 +108,33 @@ def _build_s2_segment_list(theta_cap, ring_thetas, ring_segments):
     return segments
 
 
+def _print_segmentation_info(
+    nranks, theta_cap, ring_thetas, ring_segments, precision=3
+):
+    print(f"Segmentation statistics for {nranks} ranks:")
+    print(f"  polar cap angle: {theta_cap:.{precision}f}")
+    print(f"  number of rings: {len(ring_segments)}")
+    for i in range(len(ring_segments)):
+        print(
+            f"    ring {i:3d}: {ring_segments[i]:3d} segments between "
+            f"theta=[{ring_thetas[i]:.{precision}f}, {ring_thetas[i+1]:.{precision}f}]]"
+        )
+
+
 # area imbalance
-def _print_area_imabalance(segments: List[S2Segment]):
+def _print_area_imabalance(segments: List[S2Segment], precision=3):
     areas = np.array([r.area for r in segments])
     assert np.isclose(np.sum(areas), 4 * np.pi)
+    print("  Segment area imbalance:")
+    print(f"    max/min: {np.max(areas) / np.min(areas):.{precision}f}")
+    print(f"    max/avg: {np.max(areas) / np.mean(areas):.{precision}f}")
 
-    print(f"area imbalance max/min: {np.max(areas) / np.min(areas):.3f}")
-    print(f"area imbalance max/avg: {np.max(areas) / np.mean(areas):.3f}")
 
-
-def _print_edge_to_area_ratio(segments: List[S2Segment]):
+def _print_edge_to_area_ratio(segments: List[S2Segment], precision=3):
     areas = np.array([r.area for r in segments])
     edge_lengths = np.array([r.edge_length for r in segments])
-    print(f"total edge/area ratio: {np.sum(edge_lengths) / np.sum(areas):.3f}")
+    total_ratio = np.sum(edge_lengths) / np.sum(areas)
+    print(f"  Total edge/area ratio: {total_ratio:.{precision}f}")
 
 
 class S2Partition:
@@ -147,7 +158,7 @@ class S2Partition:
         self.nranks = _nranks
         self.equal_area = equal_area
         self.theta_cap, self.ring_thetas, self.ring_segments = _s2_partition(
-            self.nranks, equal_area, verbose=verbose and self.rank == 0
+            self.nranks, equal_area
         )
         if not equal_area:
             self.ring_dtheta = self.ring_thetas[1] - self.ring_thetas[0]
@@ -161,7 +172,84 @@ class S2Partition:
         self.s2_segment = self.all_s2_segments[self.rank]
 
         if verbose and self.rank == 0:
-            print(f"\nBalance statistics for {self._nranks} ranks")
+            _print_segmentation_info(
+                self.nranks,
+                self.theta_cap,
+                self.ring_thetas,
+                self.ring_segments,
+            )
             _print_area_imabalance(self.all_s2_segments)
             _print_edge_to_area_ratio(self.all_s2_segments)
             print()
+
+
+def visualize_s2_partition(
+    nranks: int, equal_area: bool = True, use_mollweide: bool = True, fig=None
+):
+    """Visualize the S2 partitioning of the sphere.
+
+    Parameters
+    ----------
+    nranks : int
+        Number of ranks to partition the sphere into.
+    equal_area : bool, optional
+        If True, partition the sphere into equal area regions by adjusting theta.
+        Otherwise, keep delta_theta of rings constant.
+    use_mollweide : bool, optional
+        If True, use the Mollweide projection. Otherwise, use a regular plot.
+    fig : matplotlib.figure.Figure, optional
+        Figure to plot on. If None, create a new figure.
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+        Figure containing the plot.
+    matplotlib.axes.Axes
+        Axes containing the plot.
+    """
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        raise ImportError("matplotlib is required for visualization")
+
+    if fig is None:
+        fig = plt.figure()
+    if use_mollweide:
+        ax = fig.add_subplot(1, 1, 1, projection="mollweide")
+    else:
+        ax = fig.add_subplot(1, 1, 1)
+
+    theta_c, theta_f, n_segments = _s2_partition(nranks, equal_area)
+
+    for i, nsegments in enumerate(n_segments):
+        theta_lo = theta_f[i]
+        theta_hi = theta_f[i + 1]
+        if use_mollweide:
+            theta_lo -= np.pi / 2
+            theta_hi -= np.pi / 2
+
+        # draw vertical bars
+        for j in range(nsegments):
+            ax.plot(
+                (2 * np.pi / nsegments * j - np.pi) * np.ones(2),
+                [theta_lo, theta_hi],
+                color="black",
+                linewidth=0.5,
+            )
+        # draw upper ring
+        ax.axhline(theta_lo, color="black", linewidth=0.5)
+    # draw final ring (only if we have rings!)
+    if n_segments:
+        ax.axhline(theta_hi, color="black", linewidth=0.5)
+    else:
+        ax.axhline(theta_c, color="black", linewidth=0.5)
+
+    if not use_mollweide:
+        ax.set(
+            xlabel=r"$\phi$",
+            ylabel=r"$\theta$",
+            xlim=(-np.pi, np.pi),
+            ylim=(0, np.pi),
+        )
+
+    return fig, ax
